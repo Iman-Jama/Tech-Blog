dc2fdb5cac00bc7b14303b7830fb5b1c5fd57447
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-05-14 19:21:38 +0100

    commited same fixed errors branch - no changes

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
   
     if (username && password) {
-      // Send a POST request to the API endpoint
+      // Send a POST request to the API endpoint 
       const response = await fetch('/api/user/login', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
